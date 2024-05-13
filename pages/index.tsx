f66dd20e5e33f5fd70cb27a2df8b5fe01b0cc00f
impliedDistraction <joshbyrom.mobile@gmail.com>
2024-05-13 11:14:43 -0400

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from 'next/link';
 
 export default function Home() {
   return (
@@ -22,20 +23,18 @@ export default function Home() {
       </div>
 
       <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
+      <Link href="/about">
         <a
-          href="/about" // Link to your games portfolio or specific game details
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
         >
           <h2 className="mb-3 text-2xl font-semibold">
-            About{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
+            About <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">-&gt;</span>
           </h2>
           <p className="m-0 max-w-[30ch] text-sm opacity-50">
             Explore.
           </p>
         </a>
+      </Link>
 
       </div>
     </main>
