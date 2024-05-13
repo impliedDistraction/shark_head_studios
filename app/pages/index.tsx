f72ed32d4d189eef68dcc7144bf3d4327620d444
impliedDistraction <joshbyrom.mobile@gmail.com>
2024-05-13 10:22:34 -0400

    Creating structure

---
+++ app/pages/index.tsx
@@ -0,0 +1,14 @@
+import RootLayout from '../layout';  // Adjust the path as necessary
+
+const Home = () => {
+  return (
+    <RootLayout>
+      <div>
+        <h1>Welcome to Shark Head Studios</h1>
+        <p>Explore our games and projects!</p>
+      </div>
+    </RootLayout>
+  );
+};
+
+export default Home;
