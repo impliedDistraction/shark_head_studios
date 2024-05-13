352303585ee77db40b9356e105b1f8b6b838bffb
impliedDistraction <joshbyrom.mobile@gmail.com>
2024-05-13 10:00:22 -0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -33,7 +33,7 @@ export default function Home() {
             </span>
           </h2>
           <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore the worlds we've built.
+            Explore.
           </p>
         </a>
 
