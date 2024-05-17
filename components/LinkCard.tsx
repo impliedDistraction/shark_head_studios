2e38b31b99b354a5329dd522c2414935a5a3f4da
impliedDistraction <joshbyrom.mobile@gmail.com>
2024-05-17 12:18:48 -0400

    Update LinkCard.tsx

--- components/LinkCard.tsx
+++ components/LinkCard.tsx
@@ -1,6 +1,9 @@
 import React, { useMemo } from 'react';
 import Image from 'next/image';
+import Slider from 'react-slick';
 import { useRouter } from 'next/router';
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 import './LinkCard.css';
 
 const DEFAULT_IMAGE = { src: '/path/to/default/image.jpg', alt: 'Default Image', maxWidth: 500, maxHeight: 300 };
@@ -26,24 +29,35 @@ const LinkCard: React.FC<LinkCardProps> = ({ title, description, images, redirec
     return images?.length ? images : [DEFAULT_IMAGE];
   }, [images]);
 
+  const sliderSettings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: imagesToDisplay.length > 1 ? 1 : imagesToDisplay.length,
+    slidesToScroll: 1,
+    autoplay: imagesToDisplay.length > 1,
+  };
+
   return (
     <div className="link-card" onClick={() => router.push(redirectTo)}>
       <div className="image-container">
-        {imagesToDisplay.map((image, index) => (
-          <div 
-            key={`${image.src}-${index}`} 
-            className="image-wrapper" 
-            style={{ maxWidth: image.maxWidth, maxHeight: image.maxHeight }}
-          >
-            <Image 
-              src={image.src} 
-              alt={image.alt} 
-              layout="responsive" 
-              width={image.maxWidth || 500} 
-              height={image.maxHeight || 300} 
-            />
-          </div>
-        ))}
+        <Slider {...sliderSettings}>
+          {imagesToDisplay.map((image, index) => (
+            <div 
+              key={`${image.src}-${index}`} 
+              className="image-wrapper" 
+              style={{ maxWidth: image.maxWidth, maxHeight: image.maxHeight }}
+            >
+              <Image 
+                src={image.src} 
+                alt={image.alt} 
+                layout="responsive" 
+                width={image.maxWidth || 500} 
+                height={image.maxHeight || 300} 
+              />
+            </div>
+          ))}
+        </Slider>
       </div>
       <div className="content">
         <h2>{title}</h2>
