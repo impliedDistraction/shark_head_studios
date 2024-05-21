41be33ef62002d2a191942a311f2aa7a42f20d79
impliedDistraction <joshbyrom.mobile@gmail.com>
2024-05-21 13:36:35 -0400

    transpiling

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,19 @@
+import withTM from 'next-transpile-modules';
+
 /**
  * @type {import('next').NextConfig}
  */
 const nextConfig = {
-    reactStrictMode: true,
-  };
-  
-  export default nextConfig;
+  reactStrictMode: true,
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.m?js/,
+      resolve: {
+        fullySpecified: false,
+      },
+    });
+    return config;
+  },
+};
+
+export default withTM(['gsap'])(nextConfig);
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,4 @@
-// pages/_app.tsx
-
-import type { AppProps } from 'next/app';
+import { AppProps } from 'next/app';
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
